92f6bae291a654c5acf0a21adc396f236edbfdcf
RENISHAW\as154381 <as154381@renishaw.com>
2026-04-05 17:10:19 +0530

    Added mongodb and docker compose file

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ WORKDIR /app
 
 COPY . .
 
+#install all the packages specified in package*.json files
 RUN npm install
 
 EXPOSE 3000
--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 
+const app = express();
 const PORT = 3000;
+
+const mongodbURL = 'mongodb://mongo:27017/mydb';
+
+mongoose.connect(mongodbURL)
+.then(() => console.log("MongoDB connected successfully"))
+.catch(err => console.log("Cannot conect to MongoDB"));
+
+//simple schema
+const userSchema = new mongoose.Schema({
+    name : String
+});
+
+const User = mongoose.model('User', userSchema);
+
 
 //Endpoint 1 
 app.get('/', (req, res) => {
-    res.send('Hello From Docker App Home page (using Express.js)');
+    res.send('Home page \n Hello From Docker App with Express.js and MongoDB');
 });
 
 //Endpoint 2
@@ -13,13 +28,16 @@ app.get('/about', (req,res) => {
     res.send('This is about page. Explore');
 });
 
-//Endpoint 3
-app.get('/users', (req,res) =>{
-    res.json([
-        {id:1, name:'Aman'},
-        {id:2, name:'Rahul'},
-        {id:3, name:'Priya'}
-    ]);
+app.get('/add', async (req, res) => {
+   const user = new User({name : 'Aman bhai'});
+   await user.save();
+   res.send('User Added successfully');
+});
+
+//Endpoint 4
+app.get('/users', async (req,res) =>{
+   const users = await User.find();
+   res.json(users);
 });
 
 app.listen(PORT, () => {
